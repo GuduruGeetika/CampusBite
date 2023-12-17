de0cb5d0fe169fb0323bbfc8dc62bd11d66612ae
GuduruGeetika <116957523+GuduruGeetika@users.noreply.github.com>
2023-12-17 15:34:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,14 +31,10 @@ app.post('/api/orders/placeorder', async (req, res) => {
     res.json({ success: true, order: order }); // Send uniquePin in the response
   });
 
-  if (process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.use(express.static("client/build"));
-  
     app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
-  }
+
   
 app.get("/",(req,res) =>
 {
